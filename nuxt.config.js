199912bd3b7972369d25b254fd21da400342a203
Rafaela Ribeiro <rafaela.ribeiro@exatoconsultoria.com.br>
2024-01-26 18:00:44 -0300

    change Nuxt.config

--- nuxt.config.js
+++ nuxt.config.js
@@ -92,6 +92,13 @@ export default {
     ],
   ],
 
+  axios: {
+    baseURL:
+      process.env.NODE_ENV === 'production'
+        ? process.env.API_URL_PROD
+        : process.env.API_URL_DEV,
+  },
+
   router: {
     middleware: ['auth'],
   },
